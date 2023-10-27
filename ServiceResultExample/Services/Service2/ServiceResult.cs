4986564ef49ddac3d0be0bb2c3f809af13c5e02d
Alex Deane <adeane999@gmail.com>
2023-10-27 11:18:42 -0400

    Clean up and add comments

--- ServiceResultExample/Services/Service2/ServiceResult.cs
+++ ServiceResultExample/Services/Service2/ServiceResult.cs
@@ -1,22 +1,54 @@
 ﻿namespace ServiceResultExample.Services.Service2;
 
-public class ServiceResult<T>
+/// <summary>
+/// Small, naive result wrapper
+/// </summary>
+public record ServiceResult<T>
 {
+    /// <summary>
+    /// The result, of there is one
+    /// </summary>
     public T? Result { get; init; }
-    public ServiceError? Error  { get; init; }
+
+    /// <summary>
+    /// An error, if one occurred
+    /// </summary>
+    public ServiceError? Error { get; init; }
 }
 
+/// <summary>
+/// Static helper class for some syntactic sugar.
+/// This is optional for this result mechanism. 
+/// </summary>
 public static class ServiceResult
 {
+    /// <summary>
+    /// Convenience method to supply a slightly neater way
+    /// to initialize a successful result.
+    /// </summary>
     public static ServiceResult<T> ForResult<T>(T result)
         => new()
         {
             Result = result
         };
 
+    /// <summary>
+    /// For consistency, a parallel method for producing
+    /// an errored result.
+    /// </summary>
     public static ServiceResult<T> ForError<T>(ServiceError error)
         => new()
         {
+            Error = error
+        };
+
+    /// <summary>
+    /// Final convenience method for a partial success
+    /// </summary>
+    public static ServiceResult<T> ForPartialSuccess<T>(T result, ServiceError error)
+        => new()
+        {
+            Result = result,
             Error = error
         };
 }
--- ServiceResultExample/Services/Service3/ServiceResult.cs
+++ ServiceResultExample/Services/Service3/ServiceResult.cs
@@ -1,24 +1,15 @@
 ﻿namespace ServiceResultExample.Services.Service3;
 
 /// <summary>
-/// Abstraction used to return a result, error,
-/// or both
+/// Abstraction used to return a result, error, or both
 /// </summary>
-/// <param name="Result">The result object</param>
-/// <param name="Error">The error object</param>
-/// <typeparam name="TResult">Result type</typeparam>
-/// <typeparam name="TError">Error type</typeparam>
-public record ServiceResult<TResult, TError>(TResult? Result = null, TError? Error = null) where TResult : class where TError : class
+public record ServiceResult<TResult, TError>(TResult? Result = null, TError? Error = null)
+    where TResult : class where TError : class
 {
     /// <summary>
     /// Implicit cast to allow returning an instance of <see cref="TError"/>
     /// from a method
     /// </summary>
-    /// <param name="error"></param>
-    /// <returns>
-    /// A <see cref="ServiceResult{TResult,TError}"/> with only
-    /// the <see cref="ServiceResult{TResult,TError}.Error"/> property set
-    /// </returns>
     public static implicit operator ServiceResult<TResult, TError>(TError error)
         => new(Error: error);
 
@@ -26,11 +17,6 @@ public record ServiceResult<TResult, TError>(TResult? Result = null, TError? Err
     /// Implicit cast to allow returning an instance of <see cref="TResult"/>
     /// from a method
     /// </summary>
-    /// <param name="result"></param>
-    /// <returns>
-    /// A <see cref="ServiceResult{TResult,TError}"/> with only
-    /// the <see cref="ServiceResult{TResult,TError}.Result"/> property set
-    /// </returns>
     public static implicit operator ServiceResult<TResult, TError>(TResult result)
         => new(Result: result);
 
@@ -38,13 +24,30 @@ public record ServiceResult<TResult, TError>(TResult? Result = null, TError? Err
     /// Implicit cast to allow returning a Tuple of the form (<see cref="TResult"/>, <see cref="TError"/>).
     /// This is to support partial success scenarios
     /// </summary>
-    /// <param name="tuple"></param>
-    /// <returns>
-    /// A <see cref="ServiceResult{TResult,TError}"/> with both properties set
-    /// </returns>
     public static implicit operator ServiceResult<TResult, TError>(ValueTuple<TResult, TError> tuple)
         => new(Result: tuple.Item1, Error: tuple.Item2);
 
+    /// <summary>
+    /// Convenience method allowing one to switch on the result and supply
+    /// handlers for different cases. This override allows handlers to not return a value.
+    /// </summary>
+    /// <param name="successHandler">A handler to be invoked when the result is a success</param>
+    /// <param name="errorHandler">A handler to be invoked when the result is an error</param>
+    public void Switch(Action<TResult> successHandler, Action<TError> errorHandler)
+    {
+        if (Result is not null)
+            successHandler(Result);
+        else
+            errorHandler(Error!);
+    }
+
+    /// <summary>
+    /// Convenience method allowing one to switch on the result and supply
+    /// handlers for different cases and return values. This override allows handlers
+    /// to return a value.
+    /// </summary>
+    /// <param name="successHandler">A handler to be invoked when the result is a success</param>
+    /// <param name="errorHandler">A handler to be invoked when the result is an error</param>
     public T Switch<T>(Func<TResult, T> successHandler, Func<TError, T> errorHandler)
     {
         if (Result is not null)
@@ -53,7 +56,36 @@ public record ServiceResult<TResult, TError>(TResult? Result = null, TError? Err
         return errorHandler(Error!);
     }
 
-    public T Switch<T>(Func<TResult, T> successHandler, Func<TError, T> errorHandler, Func<TResult, TError, T> partialSuccessHandler)
+    /// <summary>
+    /// Convenience method allowing one to switch on the result and supply
+    /// handlers for different cases and return values, including a partial success.
+    /// This override allows the handlers to return a value.
+    /// This override allows the user to specify a handler for partial success.
+    /// </summary>
+    /// <param name="successHandler">A handler to be invoked when the result is a success</param>
+    /// <param name="errorHandler">A handler to be invoked when the result is an error</param>
+    /// <param name="partialSuccessHandler">A handler to be invoked when the result is a success <b>and</b> an error</param>
+    public void Switch(Action<TResult> successHandler, Action<TError> errorHandler,
+        Action<TResult, TError> partialSuccessHandler)
+    {
+        if (Result is not null && Error is not null)
+            partialSuccessHandler(Result, Error);
+        else if (Result is not null)
+            successHandler(Result);
+        else errorHandler(Error!);
+    }
+
+    /// <summary>
+    /// Convenience method allowing one to switch on the result and supply
+    /// handlers for different cases and return values, including a partial success.
+    /// This override allows the handlers to return a value.
+    /// This override allows the user to specify a handler for partial success.
+    /// </summary>
+    /// <param name="successHandler">A handler to be invoked when the result is a success</param>
+    /// <param name="errorHandler">A handler to be invoked when the result is an error</param>
+    /// <param name="partialSuccessHandler">A handler to be invoked when the result is a success <b>and</b> an error</param>
+    public T Switch<T>(Func<TResult, T> successHandler, Func<TError, T> errorHandler,
+        Func<TResult, TError, T> partialSuccessHandler)
     {
         if (Result is not null && Error is not null)
             return partialSuccessHandler(Result, Error);
